3aa42b6386b9b82db8343b8646d8b45a5cd06d0b
Pippin Williamson <pippin@pippinsplugins.com>
2015-02-04 12:02:22 -0600

    Move products to the top and show a list of tags for the vendor to use

--- templates/fes-email-customers-tab.php
+++ templates/fes-email-customers-tab.php
@@ -6,6 +6,17 @@
 	<fieldset>
 
 		<p>
+			<?php _e( 'Select the product(s) to include customers of for this email.', 'edd-fes-product-updates' ); ?><br/>
+			<?php foreach( edd_fes_pu_products() as $product ) : ?>
+				<label for="fes-email-product-<?php echo $product->ID; ?>">
+					<input type="checkbox" name="fes-email-products[<?php echo $product->ID; ?>]" id="fes-email-product-<?php echo $product->ID; ?>" value="<?php echo $product->ID; ?>"/>
+					<?php echo $product->post_title; ?>
+				</label><br/>
+			<?php endforeach; ?>
+
+		</p>
+
+		<p>
 			<label for="fes-email-subject"><?php _e( 'Email Subject Line', 'edd-fes-product-updates' ); ?></label>
 			<input type="text" name="fes-email-subject" id="fes-email-subject" value=""/>
 		</p>
@@ -15,15 +26,11 @@
 			<textarea name="fes-email-message" id="fes-email-message"></textarea>
 		</p>
 
-		<p>
-			<?php _e( 'Select the product(s) to include customers of for this email.', 'edd-fes-product-updates' ); ?><br/>
-			<?php foreach( edd_fes_pu_products() as $product ) : ?>
-				<label for="fes-email-product-<?php echo $product->ID; ?>">
-					<input type="checkbox" name="fes-email-products[<?php echo $product->ID; ?>]" id="fes-email-product-<?php echo $product->ID; ?>" value="<?php echo $product->ID; ?>"/>
-					<?php echo $product->post_title; ?>
-				</label><br/>
-			<?php endforeach; ?>
-
+		<p class="edd-fes-product-updates-tag-list">
+			<?php foreach ( edd_get_email_tags() as $tag ): ?>
+				<small class="edd-fes-product-updates-tag"><strong>{<?php echo $tag['tag'];?>}</strong></small>
+				<small class="edd-fes-product-updates-tag-description"><?php echo $tag['description'];?></small><br/>
+			<?php endforeach;?>
 		</p>
 
 	</fieldset>
